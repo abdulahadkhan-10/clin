646c464ac1fe32f46433eceb49dd3058ab283e61
Raheemsiddiqui527 <siddiquiraheem527@gmail.com>
2025-10-29 13:43:33 +0530

    CHANGES BY RAHEEM

---
+++ clin/src/app/consulting/page.jsx
@@ -0,0 +1,16 @@
+import AboutSection from '@/component/consulting/about/page'
+import FeaturesSection from '@/component/consulting/featuresection/feature'
+import ConsultingSection from '@/component/consulting/page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ConsultingSection/>
+      <AboutSection/>
+      <FeaturesSection/>
+    </div>
+  )
+}
+
+export default page
